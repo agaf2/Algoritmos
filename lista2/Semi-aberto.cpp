29099a88420c396287fcc2de095e131bd65cd796
agaf2 <agaf2@cin.ufpe.br>
2023-07-07 16:07:44 -0300

    pax mundial

---
+++ lista2/Semi-aberto.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//using namespace std;
+
+//struct Floor
+//{
+    
+
+//};
+
+
+int main(){
+
+int m;
+
+    //Floor 
+    scanf("%d", &m);
+    int andares[m];
+
+
+
+    return 0;
+}
